5fb5e1e99318962c4b7aa45c1e66c443a03dbd27
Shoham Margalit <75728931+shoham404@users.noreply.github.com>
2025-02-02 12:41:05 +0200

    Update Cost.js

--- models/Cost.js
+++ models/Cost.js
@@ -1,15 +1,3 @@
-/*/**
- * Mongoose model for storing user expenses (costs).
- * This schema defines the structure of the cost documents in the database.
- * Each document represents an expense made by a user, including details like description, category, amount, and date.
- *
- * @typedef {Object} Cost
- * @property {string} description - A brief description of the expense (e.g., "Groceries", "Utility Bill").
- * @property {string} category - The category of the expense (e.g., "food", "health", "housing").
- * @property {string} userid - The ID of the user who made the expense.
- * @property {number} sum - The total amount of the expense, must be a positive number (minimum: 0).
- * @property {Date} date - The date the expense was made. Defaults to the current date if not provided.
- */
 /**
  * Cost schema for storing user costs.
  *
